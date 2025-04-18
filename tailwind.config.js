942699b456fbd1bfc12f97a8755295d55b20521a
Moaru <77356600+moarunocounter@users.noreply.github.com>
2025-04-18 11:48:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",        // ini penting!
+    "./components/**/*.{js,ts,jsx,tsx}", // juga ini
+    "./lib/**/*.{js,ts,jsx,tsx}",        // kalau kamu gunakan folder lib
   ],
   theme: {
     extend: {},
